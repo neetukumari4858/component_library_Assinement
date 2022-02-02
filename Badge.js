a2d47f890bff500e87c0bee2a5c1debea26d2ba1
neetukumari4858 <neetu21@navgurukul.org>
2022-02-02 15:37:41 +0530

    initial commit
    initial commit

---
+++ Badge.js
@@ -0,0 +1,14 @@
+let badgebtn=document.getElementById("btn2")
+let badge=document.querySelector(".badges4")
+badgebtn.addEventListener("click",badgeHide=()=>{
+    if(badge.style.visibility==visible){
+        badge.style.display=none;
+    }
+    else{
+        badge.style.display="none"
+    }
+})
+
+
+
+  
